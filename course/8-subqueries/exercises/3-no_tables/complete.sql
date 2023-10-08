eacbe8bdaaa23b4aaa8491f6555392df69067cd5
Ram Charan Vakkalanka <ramcharanvakkalanka@gmail.com>
2023-10-08 17:11:41 +0530

    9-normalization

--- course/8-subqueries/exercises/3-no_tables/complete.sql
+++ course/8-subqueries/exercises/3-no_tables/complete.sql
@@ -1,4 +1,8 @@
-SELECT * FROM users
+SELECT *
+FROM users
 WHERE age_in_days > (
-  SELECT 365 * 40
-)
+    SELECT 365 * 40
+  ) -- Can't we just write like this ? =>
+SELECT *
+FROM users
+WHERE age_in_days > 365 * 40;
